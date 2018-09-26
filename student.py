704c13a6a42091ebcf9e2faf8694f944a2325d35
Christian Fowlkes <fowlkesc@gilmour.org>
2018-09-26 15:16:22 -0400

    change midpoint and motor speed

--- student.py
+++ student.py
@@ -23,9 +23,9 @@ class Piggy(pigo.Pigo):
         self.SAFE_STOP_DIST = 30
         self.HARD_STOP_DIST = 15
         # YOU DECIDE: What left motor power helps straighten your fwd()?
-        self.LEFT_SPEED = 135
+        self.LEFT_SPEED = 140
         # YOU DECIDE: What left motor power helps straighten your fwd()?
-        self.RIGHT_SPEED = 140
+        self.RIGHT_SPEED = 155
         # This one isn't capitalized because it changes during runtime, the others don't
         self.turn_track = 0
         # Our scan list! The index will be the degree and it will store distance
