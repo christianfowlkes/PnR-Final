fb5a8de11a1974cea6cd48c7a7b761947824ec81
Christian Fowlkes <fowlkesc@gilmour.org>
2018-10-03 14:22:08 -0400

    fixed line 62

--- student.py
+++ student.py
@@ -59,7 +59,7 @@ class Piggy(pigo.Pigo):
     # YOU DECIDE: How does your GoPiggy dance?
     def dance(self):
         """executes a series of methods that add up to a compound dance"""
-        if not self.safe to dance():
+        if not self.safe_to_dance():
             print("n\----NOT SAFE TO DANCE----\n")
             return
         print("\n---- LET'S DANCE ----\n")
