8526c8b675d789121e9024decf544a8d5a979acc
Christian Fowlkes <fowlkesc@gilmour.org>
2018-11-28 14:19:10 -0500

    changed nav method

--- student.py
+++ student.py
@@ -232,7 +232,7 @@ class Piggy(pigo.Pigo):
                 self.cruise()
             else:
                 while not self.is_clear():
-                    self.encR(6)
+                    self.choose_path()
 
     def cruise(self):
         """ drive straight while path is clear """
